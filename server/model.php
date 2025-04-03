b401158ca6d0f5fbcc3bff484efe6990b7e0204a
Reich Malo <reich2@mmi-dev.unilim.fr>
2025-04-03 09:17:42 +0200

    premiére modif test

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "reich2");
+define("DBLOGIN", "reich2");
+define("DBPWD", "reich2");
